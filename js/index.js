8047d59bc36827cae1dc36621b556f573fc64e94
Ane Spinola <ane2021ucf@gmail.com>
2021-07-15 23:48:28 -0400

    update 2

---
+++ js/index.js
@@ -0,0 +1,14 @@
+console.log ("Your index file is loaded correctly!")
+
+$( "li" ).hover(
+    function() {
+      $( this ).append( $( "<span> ***</span>" ) );
+    }, function() {
+      $( this ).find( "span" ).last().remove();
+    }
+  );
+   
+  $( "li.fade" ).hover(function() {
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn( 500 );
+  });
